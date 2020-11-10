e71fd48a075f5188886a094d197879dd267acf22
KuiMing <benjamin0901@gmail.com>
2020-11-10 10:01:39 +0800

    get project list

--- train_image_classification.py
+++ train_image_classification.py
@@ -52,8 +52,19 @@ def main():
     credentials = ApiKeyCredentials(in_headers={"Training-key": config["training_key"]})
     trainer = CustomVisionTrainingClient(config["ENDPOINT"], credentials)
     # Create a new project
+
     print("Creating project...")
-    project = trainer.create_project(config["project_name"])
+    try:
+        project = trainer.create_project(config["project_name"])
+        project_id = project.id
+    except:
+        project_list = trainer.get_projects()
+        projects = {}
+        for i in project_list:
+            temp = i.as_dict()
+            projects[temp["name"]] = temp["id"]
+            projects[temp["name"]] = temp["id"]
+        project_id = projects[config["project_name"]]
     publish_iteration_name = config["publish_iteration_name"]
     prediction_resource_id = config["prediction_resource_id"]
     # Make two tags in the new project
@@ -62,27 +73,30 @@ def main():
 
     print("Adding images...")
     image_list = []
-    for i in args.tag:
-        image_list += add_image(i, base_image_url, project.id, trainer)
+    for i in config["tag"]:
+        image_list += add_image(i, base_image_url, project_id, trainer)
 
     upload_result = trainer.create_images_from_files(
-        project.id, ImageFileCreateBatch(images=image_list)
+        project_id, ImageFileCreateBatch(images=image_list)
     )
     if not upload_result.is_batch_successful:
         print("Image batch upload failed.")
         for image in upload_result.images:
             print("Image status: ", image.status)
-        exit(-1)
 
     print("Training...")
-    iteration = trainer.train_project(project.id)
+    iteration = trainer.train_project(project_id)
     while iteration.status != "Completed":
-        iteration = trainer.get_iteration(project.id, iteration.id)
+        iteration = trainer.get_iteration(project_id, iteration.id)
         print("Training status: " + iteration.status)
         time.sleep(1)
 
     # The iteration is now trained. Publish it to the project endpoint
     trainer.publish_iteration(
-        project.id, iteration.id, publish_iteration_name, prediction_resource_id
+        project_id, iteration.id, publish_iteration_name, prediction_resource_id
     )
     print("Done!")
+
+
+if __name__ == "__main__":
+    main()
