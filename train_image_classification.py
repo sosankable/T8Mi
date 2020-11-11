14605186a6c231f37459167092d919a87114589c
Benjamin Chen <benjamin.chen@momagic.com>
2020-11-11 16:43:47 +0800

    modify project

--- train_image_classification.py
+++ train_image_classification.py
@@ -9,7 +9,6 @@ from azure.cognitiveservices.vision.customvision.training import (
 from azure.cognitiveservices.vision.customvision.training.models import (
     ImageFileCreateBatch,
     ImageFileCreateEntry,
-    CustomVisionErrorException,
 )
 from msrest.authentication import ApiKeyCredentials
 
@@ -30,10 +29,10 @@ def parse_args():
     return args
 
 
-def add_image(tag, base_image_url, project_id, trainer):
+def add_image(label, base_image_url, project_id, trainer):
     image_list = []
-    image_tag = trainer.create_tag(project_id, tag)
-    filenames = glob.glob(os.path.join(base_image_url, tag, "*.jpg"))
+    image_tag = trainer.create_tag(project_id, label)
+    filenames = glob.glob(os.path.join(base_image_url, label, "*.jpg"))
     for file_name in filenames:
         with open(file_name, "rb") as image_contents:
             image_list.append(
@@ -54,46 +53,38 @@ def main():
     # Create a new project
 
     print("Creating project...")
-    try:
-        project = trainer.create_project(config["project_name"])
-        project_id = project.id
-    except CustomVisionErrorException:
-        project_list = trainer.get_projects()
-        projects = {}
-        for i in project_list:
-            temp = i.as_dict()
-            projects[temp["name"]] = temp["id"]
-            projects[temp["name"]] = temp["id"]
-        project_id = projects[config["project_name"]]
-    publish_iteration_name = config["publish_iteration_name"]
-    prediction_resource_id = config["prediction_resource_id"]
-    # Make two tags in the new project
+    project = trainer.create_project(config["project_name"])
 
-    base_image_url = config["image_folder"]
+    # ======================================================================================
+    # Make two labels in the new project
 
     print("Adding images...")
+    base_image_url = config["image_folder"]
     image_list = []
-    for i in config["tag"]:
-        image_list += add_image(i, base_image_url, project_id, trainer)
+    for i in config["label"]:
+        image_list += add_image(i, base_image_url, project.id, trainer)
 
     upload_result = trainer.create_images_from_files(
-        project_id, ImageFileCreateBatch(images=image_list)
+        project.id, ImageFileCreateBatch(images=image_list)
     )
     if not upload_result.is_batch_successful:
         print("Image batch upload failed.")
         for image in upload_result.images:
             print("Image status: ", image.status)
 
+    # ======================================================================================
     print("Training...")
-    iteration = trainer.train_project(project_id)
+    publish_iteration_name = config["publish_iteration_name"]
+    prediction_resource_id = config["prediction_resource_id"]
+    iteration = trainer.train_project(project.id)
     while iteration.status != "Completed":
-        iteration = trainer.get_iteration(project_id, iteration.id)
+        iteration = trainer.get_iteration(project.id, iteration.id)
         print("Training status: " + iteration.status)
         time.sleep(1)
 
     # The iteration is now trained. Publish it to the project endpoint
     trainer.publish_iteration(
-        project_id, iteration.id, publish_iteration_name, prediction_resource_id
+        project.id, iteration.id, publish_iteration_name, prediction_resource_id
     )
     print("Done!")
 
