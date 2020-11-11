d7857ea0f197b68569a9cb111e70874d9a1f9cd1
Benjamin Chen <benjamin.chen@momagic.com>
2020-11-11 16:44:48 +0800

    training for object detection

---
+++ train_object_detection.py
@@ -0,0 +1,105 @@
+import argparse
+import json
+import os
+import time
+from azure.cognitiveservices.vision.customvision.training import (
+    CustomVisionTrainingClient,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateBatch,
+    ImageFileCreateEntry,
+    Region,
+)
+from msrest.authentication import ApiKeyCredentials
+
+
+def parse_args():
+    """
+    Parse argument
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="cofigure file path",
+        type=str,
+        default="object_detection_config.json",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def add_image(trainer, label, project_id, annotation, base_image_url):
+    tagged_images_with_regions = []
+    tag = trainer.create_tag(project_id, label)
+    for file_name in annotation.keys():
+        left, top, width, height = annotation[file_name]
+        regions = [
+            Region(tag_id=tag.id, left=left, top=top, width=width, height=height)
+        ]
+        file_path = os.path.join(base_image_url, label, file_name + ".jpg")
+        with open(file_path, "rb") as image_contents:
+            tagged_images_with_regions.append(
+                ImageFileCreateEntry(
+                    name=file_name, contents=image_contents.read(), regions=regions
+                )
+            )
+    return tagged_images_with_regions
+
+
+def main():
+    args = parse_args()
+    config = json.load(open(args.config, "r"))
+    credentials = ApiKeyCredentials(in_headers={"Training-key": config["training_key"]})
+    trainer = CustomVisionTrainingClient(config["ENDPOINT"], credentials)
+
+    print("Creating project...")
+
+    # Find the object detection domain
+    obj_detection_domain = next(
+        domain
+        for domain in trainer.get_domains()
+        if domain.type == "ObjectDetection" and domain.name == "General"
+    )
+    project = trainer.create_project(
+        config["project_name"], domain_id=obj_detection_domain.id
+    )
+
+    # ======================================================================================
+
+    print("Adding images...")
+    base_image_url = config["image_folder"]
+    annotations = json.load(open("annotation.json", "r"))
+    tagged_images_with_regions = []
+    for label in annotations.keys():
+        tagged_images_with_regions += add_image(
+            trainer, label, project.id, annotations[label], base_image_url
+        )
+
+    upload_result = trainer.create_images_from_files(
+        project.id, ImageFileCreateBatch(images=tagged_images_with_regions)
+    )
+    if not upload_result.is_batch_successful:
+        print("Image batch upload failed.")
+        for image in upload_result.images:
+            print("Image status: ", image.status)
+
+    # ======================================================================================
+    print("Training...")
+    publish_iteration_name = config["publish_iteration_name"]
+    prediction_resource_id = config["prediction_resource_id"]
+    iteration = trainer.train_project(project.id)
+    while iteration.status != "Completed":
+        iteration = trainer.get_iteration(project.id, iteration.id)
+        print("Training status: " + iteration.status)
+        time.sleep(1)
+
+    # The iteration is now trained. Publish it to the project endpoint
+    trainer.publish_iteration(
+        project.id, iteration.id, publish_iteration_name, prediction_resource_id
+    )
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
