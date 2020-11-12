52d85930d09855d551cdabb42b206e6070470d39
Benjamin Chen <benjamin.chen@momagic.com>
2020-11-12 18:43:17 +0800

    prediction for mnist

---
+++ predict_mnist_azml.py
@@ -0,0 +1,60 @@
+import argparse
+import gzip
+import json
+import os
+import requests
+import numpy as np
+from PIL import Image
+
+
+def load_image(path):
+    f = gzip.open(path, "r")
+    image_size = 28
+    f.read(16)
+    buf = f.read()
+    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    data = data.reshape(int(data.shape[0] / 28 / 28), image_size, image_size, 1)
+    f.close()
+    return data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e", "--endpoint_url", type=str, help="Path to the training data"
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+
+    args = parse_args()
+    # Prepare the testing data
+    test_image = load_image(os.path.join(args.data_folder, "t10k-images-idx3-ubyte.gz"))
+    test_image /= 255
+    testing_num = np.random.randint(low=0, high=len(test_image) - 1)
+
+    # Two sets of data to score, so we get two results back
+    data = {"data": test_image[testing_num].tolist()}
+    # Convert to JSON string
+    input_data = json.dumps(data)
+
+    # Set the content type
+    headers = {"Content-Type": "application/json"}
+
+    # Make the request and display the response
+    resp = requests.post(args.endpoint_url, input_data, headers=headers)
+
+    ans = resp.text.replace("[", "").replace("]", "").split(", ")
+    ans = [float(i) for i in ans]
+    print(np.argmax(ans))
+    array = np.reshape(test_image[testing_num] * 255, (28, 28))
+    img = Image.fromarray(array)
+    img.show()
+    img = img.convert("RGB")
+    img.save("output.png")
+
+
+if __name__ == "__main__":
+    main()
---
+++ predict_mnist_local.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import gzip
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+
+def load_image(path):
+    f = gzip.open(path, "r")
+    image_size = 28
+    f.read(16)
+    buf = f.read()
+    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    data = data.reshape(int(data.shape[0] / 28 / 28), image_size, image_size, 1)
+    f.close()
+    return data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--data_folder", type=str, help="Path to the training data"
+    )
+    parser.add_argument("-m", "--model", type=str, help="Path of model")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    # Prepare the testing data
+    test_image = load_image(os.path.join(args.data_folder, "t10k-images-idx3-ubyte.gz"))
+    test_image /= 255
+    testing_num = np.random.randint(low=0, high=len(test_image) - 1)
+
+    # Load the model and predict
+    model = load_model(args.model)
+    predict = model.predict(test_image[testing_num].reshape(1, 28, 28, 1))
+    # Output the prediction and show the testing image
+    print(np.argmax(predict))
+    array = np.reshape(test_image[testing_num] * 255, (28, 28))
+    img = Image.fromarray(array)
+    img.show()
+    img = img.convert("RGB")
+    img.save("output.png")
+
+
+if __name__ == "__main__":
+    main()
