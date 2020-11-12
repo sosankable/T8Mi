a798423d345731a7c440377864a1510ab74fb679
Benjamin Chen <benjamin.chen@momagic.com>
2020-11-12 18:35:15 +0800

    simple experiment

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,29 @@
-import tensorflow as tf
-from azureml.core import Environment
-from azureml.core.conda_dependencies import CondaDependencies
-from azureml.core import Model
+"""
+Hello on Azure machine learning.
+"""
+
+from azureml.core import Workspace, Experiment, ScriptRunConfig
+import azureml
 
 
-environment = Environment("my-torch-environment")
-environment.python.conda_dependencies = CondaDependencies.create(
-    pip_packages=["azureml-defaults", "tensorflow=={}".format(tf.__version__), "numpy"]
-)
+def main():
+    """
+    Hello on Azure machine learning.
+    """
+    work_space = Workspace.from_config()
+
+    experiment = Experiment(workspace=work_space, name="hello-experiment")
+
+    config = ScriptRunConfig(
+        source_directory=".", script="hello.py", compute_target="cpu-cluster"
+    )
+    azureml._restclient.snapshots_client.SNAPSHOT_MAX_SIZE_BYTES = 2000000000
+
+    run = experiment.submit(config)
+    aml_url = run.get_portal_url()
+    print(aml_url)
+    run.wait_for_completion(show_output=True)
+
+
+if __name__ == "__main__":
+    main()
